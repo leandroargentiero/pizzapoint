fd74062f847e8f187c74ff27a04dcdd432c755b1
Leandro Argentiero <argentiero.leo@gmail.com>
2022-03-31 17:56:40 +0200

    Installed Chakra UI

---
+++ styles/theme.js
@@ -0,0 +1,21 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  // colors: {
+  //   brand: {
+  //     bg: '#E4DED7',
+  //     text: '#144149',
+  //     accent: '#FC6349',
+  //   },
+  // },
+  // styles: {
+  //   global: {
+  //     'html, body': {
+  //       backgroundColor: 'brand.bg',
+  //       color: 'brand.text',
+  //     },
+  //   },
+  // },
+});
+
+export default theme;
